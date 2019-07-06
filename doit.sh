c333f8a3d057d7e62adff529a9ff4a06988d4bea
Mario <morteg91@unm.edu>
2019-07-06 08:52:12 -0700

    Added dissertation compile script

---
+++ doit.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
+bibtex thesis 
+pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
+pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
+
+open -a Preview thesis.pdf
+
+
+#pdflatex thesis && \
+#bibtex thesis && \
+#pdflatex thesis && \
+#pdflatex thesis && \
+#open -a Preview thesis.pdf
+
+#latex thesis && \
+#biber thesis && \
+#latex thesis
+
+# with biblatex, it is only necessary to run latex once after bibtex, not twice.
+# with older tex installations, you may need to change 'biber' to 'bibtex'
