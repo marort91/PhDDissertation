03865abab7e57301d189245f3c3114b47a8f13ac
Mario I Ortega <marioiortega@Marios-MacBook-Pro-2.local>
2019-07-07 09:49:14 -0700

    Compiling on new laptop. Some inputenc errors but they aren't fatal. Will deal with them later.

--- doit.sh
+++ doit.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
-bibtex thesis 
-pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
-pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
+#pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
+#bibtex thesis 
+#pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
+#pdflatex -interaction nonstopmode -halt-on-error -file-line-error thesis
 
-open -a Preview thesis.pdf
-
-
-#pdflatex thesis && \
-#bibtex thesis && \
-#pdflatex thesis && \
-#pdflatex thesis && \
 #open -a Preview thesis.pdf
+
+pdflatex -interaction scrollmode thesis 
+bibtex thesis 
+pdflatex -interaction scrollmode thesis 
+pdflatex -interaction scrollmode thesis 
+open -a Preview thesis.pdf
 
 #latex thesis && \
 #biber thesis && \
